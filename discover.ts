ee2750ccfd8ae5a3b5c968a68a5044e56e56ad9f
Camel Djoulako <cameldjoulako17@gmail.com>
2025-11-06 18:39:20 +0100

    fix error on code. set correct type

--- discover.ts
+++ discover.ts
@@ -12,3 +12,10 @@ function damage(characterDamage: { life: number }, amount: number): number {
 
 const result2 = damage({ life: 100 }, 12);
 console.log(result);
+
+// Ne modifiez que le typage TypeScript, ne touchez pas au code JavaScript !
+
+function sayHello(target: { firstName: string; money: number }): string {
+  return `Hello ${target.firstName}, you have ${target.money} on your bank account.`;
+}
+const message: string = sayHello({ firstName: "World", money: 123 });
