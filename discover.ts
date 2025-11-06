74ec7f27f4751d2da630d2d82ad8cc3e1fabe81e
Camel Djoulako <cameldjoulako17@gmail.com>
2025-11-06 18:34:23 +0100

    objet type

--- discover.ts
+++ discover.ts
@@ -3,3 +3,12 @@ function addition(x: number, y: number): number {
 }
 const result: number = addition(10, 20);
 console.log(result);
+
+//object type
+function damage(characterDamage: { life: number }, amount: number): number {
+  characterDamage.life -= amount;
+  return characterDamage.life;
+}
+
+const result2 = damage({ life: 100 }, 12);
+console.log(result);
